13dfb7bc2bdd7ae99b71546be85065c05958b290
gumie <gumie3x1@gmail.com>
2024-05-14 15:33:26 +0700

    update file location

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
+  return (  
+    <body class="bg-white rounded-lg py-5">
+      <div class="container flex flex-col mx-auto bg-white rounded-lg pt-12 my-5">
+        <div class="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
+        <div class="flex items-center xl:p-10">
+          
+        </div>
+        </div>
+      </div>
+    </body>
   )
 }
 
