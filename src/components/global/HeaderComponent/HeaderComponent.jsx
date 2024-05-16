d9e3a9c305ae1a124a0b7833d3a9cf2b0ee8b976
gumie <gumie3x1@gmail.com>
2024-05-16 15:08:10 +0700

    fix header

--- src/components/global/HeaderComponent/HeaderComponent.jsx
+++ src/components/global/HeaderComponent/HeaderComponent.jsx
@@ -1,92 +1,91 @@
-import React, { useState } from 'react';
-import Logo from './Header/logo.jpg';
-import HeaderBackground from './Header/nenHeader.jpg';
-import NhuongQuyen from './Header/Nhuongquyen.jpg';
-import { LocalGroceryStore, LocationOn, MailOutline, Menu, PhoneInTalk, Search } from '@mui/icons-material';
-import SidebarComponent from '../Sidebar/SidebarComponent';
+import React, { useState } from "react";
+import Logo from "./Header/logo.jpg";
+import HeaderBackground from "./Header/nenHeader.jpg";
+import NhuongQuyen from "./Header/Nhuongquyen.jpg";
+import {
+  LocalGroceryStore,
+  LocationOn,
+  MailOutline,
+  Menu,
+  PhoneInTalk,
+  Search,
+} from "@mui/icons-material";
+import SidebarComponent from "../Sidebar/SidebarComponent";
 
 const HeaderComponent = () => {
-    const LogoHeader = {
-        backgroundImage: `url(${Logo})`,
-        backgroundSize: 'contain',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        width: '314px',
-        height: '42px',
-        paddingTop: '4px',
-        paddingBottom: '4px',
-    };
+  const HeaderBackGround = {
+    backgroundImage: `url(${HeaderBackground})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    paddingTop: "5px",
+    paddingBottom: "5px",
+    height: "50%",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+  };
 
-    const NhuongQuyenLogo = {
-        backgroundImage: `url(${NhuongQuyen})`,
-        backgroundSize: 'contain',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        width: '232px',
-        height: '40px',
-        marginLeft: '120px'
-    };
+  const LogoHeader = {
+    backgroundImage: `url(${Logo})`,
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    width: "314px",
+    height: "42px",
+    paddingTop: "4px",
+    paddingBottom: "4px",
+  };
 
-    const HeaderBackGround = {
-        backgroundImage: `url(${HeaderBackground})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        paddingTop: '5px',
-        paddingBottom: '5px',
-        height: '52px',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-    };
+  const NhuongQuyenLogo = {
+    backgroundImage: `url(${NhuongQuyen})`,
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    width: "230px",
+    height: "40px",
+    marginLeft: "0px",
+  };
 
-    const [openSidebar, setOpenSidebar] = useState(false);
-
-
-    const handleOpenSidebar = () => {
-        setOpenSidebar(true);
-    };
-
-    const handleCloseSidebar = () => {
-        setOpenSidebar(false);
-    };
-
-    return (
-        <>
-            <div style={HeaderBackGround}>
-                <div style={{ paddingLeft: '15px', display: 'flex', alignItems: 'center' }}>
-                    <div style={LogoHeader}></div>
-                    <div style={NhuongQuyenLogo}></div>
-                    <div style={{ display: 'flex', alignItems: 'center', marginLeft: '223px' }}>
-                        <span style={{ marginRight: '72px', width: '126px' }}>
-                            <PhoneInTalk style={{ color: 'white', fontSize: '14px' }} />
-                            <span style={{ color: 'white', marginLeft: '5px', fontSize: '14px' }}>024 3856 3856</span>
-                        </span>
-                        <span style={{ marginRight: '72px', width: '189px' }}>
-                            <MailOutline style={{ color: 'white', fontSize: '14px' }} />
-                            <span style={{ color: 'white', marginLeft: '5px', fontSize: '14px' }}>cskh@freshgarden.vn</span>
-                        </span>
-                        <span style={{ width: '171px' }}>
-                            <LocationOn style={{ color: 'white', fontSize: '14px' }} />
-                            <span style={{ color: 'white', marginLeft: '5px', fontSize: '14px' }}>46 An Dương, Hà Nội</span>
-                        </span>
-                    </div>
-                    <div style={{ display: 'flex', alignItems: 'center', marginLeft: '213px' }}>
-                        <span style={{ marginRight: '32.25px' }}>
-                            <Search style={{ color: 'white', fontSize: '21px' }} />
-                        </span>
-                        <span style={{ marginRight: '32.25px' }}>
-                            <LocalGroceryStore style={{ color: 'white', fontSize: '21px' }} />
-                        </span>
-                        <span onClick={handleOpenSidebar} style={{ cursor: 'pointer' }}>
-                            <Menu style={{ color: 'white', fontSize: '21px' }} />
-                        </span>
-                    </div>
-                    <SidebarComponent open={openSidebar} onClose={handleCloseSidebar} />
-                </div>
+  return (
+    <>
+      <div
+        className="container-fluid justify-center items-center"
+        style={{ height: "100px", maxHeight: "80vh" }}
+      >
+        <div className="bg-fixed justify-content-between" style={HeaderBackGround}>
+          <div className="mr-10" style={LogoHeader}></div>
+          <div style={NhuongQuyenLogo}></div>
+          <div className="text-white text-sm ml-20 flex items-center space-x-6">
+            <div className="flex items-center space-x-2">
+              <PhoneInTalk fontSize="14px" />
+              <a className="cursor-pointer">024 3856 3856</a>
             </div>
-        </>
-    );
+            <div className="flex items-center space-x-2">
+              <MailOutline fontSize="14px" />
+              <a className="cursor-pointer">cskh@freshgarden.vn</a>
+            </div>
+            <div className="flex items-center space-x-2">
+              <LocationOn fontSize="14px" />
+              <a className="cursor-pointer">46 An Dương, Hà Nội</a>
+            </div>
+          </div>
+
+          <div className="text-white flex justify-content-end ml-auto">
+            <a className="cursor-pointer px-5">
+              <Search fontSize="medium"/>
+            </a>
+            <span className="cursor-pointer px-5">
+              <LocalGroceryStore fontSize="medium"/>
+            </span>
+            <span onClick="" className="cursor-pointer px-5">
+              <Menu fontSize="medium"/>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HeaderComponent;
--- src/pages/admin/users/UserList.jsx
+++ src/pages/admin/users/UserList.jsx
@@ -46,10 +46,10 @@ const UserList = () => {
                     <table className="border-collapse bg-white text-left text-sm text-gray-500 w-full">
                         <thead className="bg-gray-50">
                             <tr>
-                                <th scope="col" className="px-6 py-4 font-medium text-gray-900">Name</th>
-                                <th scope="col" className="px-6 py-4 font-medium text-gray-900">State</th>
-                                <th scope="col" className="px-6 py-4 font-medium text-gray-900">Role</th>
-                                <th scope="col" className="px-6 py-4 font-medium text-gray-900">Team</th>
+                                <th scope="col" className="px-6 py-4 font-medium text-gray-900">Tên</th>
+                                <th scope="col" className="px-6 py-4 font-medium text-gray-900">Trạng thái</th>
+                                <th scope="col" className="px-6 py-4 font-medium text-gray-900">Phân Quyền</th>
+                                <th scope="col" className="px-6 py-4 font-medium text-gray-900"></th>
                                 <th scope="col" className="px-6 py-4 font-medium text-gray-900"></th>
                             </tr>
                         </thead>
