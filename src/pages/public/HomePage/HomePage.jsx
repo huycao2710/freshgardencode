36a4c4a0d47ab79f71b4b70895eb7057aa725c5f
huycao2710 <caovanhuy2710@gmail.com>
2024-05-19 23:54:44 +0700

    Remove Header in HomePage

--- src/pages/public/HomePage/HomePage.jsx
+++ src/pages/public/HomePage/HomePage.jsx
@@ -12,42 +12,14 @@ const HomePage = () => {
 
   return (
     <div>
-<<<<<<< HEAD
-      <div id="homeslider" className="h-screen">
-        <MainCarousel />
-      </div>
-      <section className="home-featured-product py-12" style={{ backgroundImage: 'url(/assets/images/Header/nenHeader.jpg)' }}>
-        <MainProduct />
-      </section>
-      <section className="-home-tab-product py-12" >
-        <Product />
-      </section>
-      <section>
-        <div>About</div>
-      </section>
-      <section>
-        <div>About2</div>
-      </section>
-      <section>
-        <div>articles</div>
-      </section>
-      <section>
-        <div>client</div>
-      </section>
-      <section>
-        <div>brand</div>
-      </section>
-=======
-      <HeaderComponent />
       <MainCarousel />
       <MainProduct />
       <Product />
       <About />
       <About2 />
       <Service />
-      <Articles/>
+      <Articles />
       <ClientReview />
->>>>>>> d3c22de1a9ab93cca9a7c8caace7b602f807067d
     </div>
   );
 };
