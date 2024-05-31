caf1c4732aeef1e87d01a71dd4b5a54db1b04122
gumie <gumie3x1@gmail.com>
2024-05-31 21:57:15 +0700

    update footer

--- src/pages/public/HomePage/HomePage.jsx
+++ src/pages/public/HomePage/HomePage.jsx
@@ -7,6 +7,7 @@ import About2 from "../../../components/global/MainProduct/About2";
 import Service from "../../../components/global/MainProduct/Service";
 import Articles from "../../../components/global/MainProduct/Articles";
 import ClientReview from "../../../components/global/MainProduct/ClientReview";
+import Footer from "../../../components/global/FooterComponent/Footer";
 
 const HomePage = () => {
   return (
@@ -22,7 +23,7 @@ const HomePage = () => {
       <ClientReview />
     </div>
     <footer>
-
+      <Footer/>
     </footer>
     </>
     
