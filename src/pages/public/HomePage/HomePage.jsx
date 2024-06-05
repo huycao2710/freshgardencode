7c70c58fe27f6f6afd3fea023eea3a9f4e1c4aa9
huycao2710 <caovanhuy2710@gmail.com>
2024-06-05 17:15:15 +0700

    update homepage

--- src/pages/public/HomePage/HomePage.jsx
+++ src/pages/public/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import MainCarousel from "../../../components/global/MainCarousel/MainCarousel";
 import MainProduct from "../../../components/global/MainProduct/MainProduct";
-import Product from "../../../components/global/MainProduct/Product";
 import About from "../../../components/global/MainProduct/About";
 import About2 from "../../../components/global/MainProduct/About2";
 import Service from "../../../components/global/MainProduct/Service";
@@ -14,6 +13,7 @@ const HomePage = () => {
       <div>
         <MainCarousel />
         <MainProduct />
+        <About />
         <About2 />
         <Service />
         <Articles />
